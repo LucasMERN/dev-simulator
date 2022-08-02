5661195e90c978e594fedd90b801bfed2d7bd536
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-08-02 18:07:45 -0500

    deleted index.js file and created a Home Directory

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WINKLISTER</h1>
+      </header>
+      <h2>Multiple Roomates</h2>
+      <h2>Multiple Bills</h2>
+      <h2>One App</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+// Styled component named Dotol
+export const Dotol = styled.dot`
+  background-color: green;
+  color: green;
+`;
