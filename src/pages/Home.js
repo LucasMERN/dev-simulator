ce6d616e9cce21b298a644652973e6a1802d5d5b
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-10-09 16:05:22 -0500

    replaced index.js with a new dir and updated new dir's content

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+      <header className="App-header">
+        Welcome to your Developer Simulation!
+      </header>
+  );
+}
+
+export default App;
