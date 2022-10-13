3223ccc1c3a1185edd28337b8ceb861afe1a1db0
Lucas Winkler <winkler.lucas1995@gmail.com>
2022-10-12 19:50:38 -0500

    refactored file structure for component dir and added theme file in the root dir.

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Title as StyledTitle } from './style'
+
+function Title() {
+    return (
+        <StyledTitle>
+          This is a title
+        </StyledTitle>
+    );
+  }
+
+export default Title;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,10 @@
 import '../App.css';
-import styled from "styled-components"
+import Title from '../Components/Title/Title'
 import React from "react"
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
 
 function Home() {
   return (
-      <Title>
-        This is a title
-      </Title>
+      <Title />
   );
 }
 
